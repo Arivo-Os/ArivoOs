a533c68809f4c0a172215900f50f421985d2374c
Akhilesh goswami <akhileshgoswami@nothing.attdns.com>
2026-06-29 18:30:17 +0530

    remove proxy api code

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
   images: {
     unoptimized: true,
     formats: ["image/avif", "image/webp"],
-  },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups",
-          },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "unsafe-none",
-          },
-        ],
-      },
-    ];
   },
 };
 
